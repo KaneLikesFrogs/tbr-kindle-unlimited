d3f4575dacc237990e0ef8628ce66f2bbbec84c4
Kane <KaneHodgkinson@outlook.com>
2025-12-16 22:56:03 +0000

    Added a loading bar to console output, improved general handling of program and reduce waits throughout

--- storygraphKU.py
+++ storygraphKU.py
@@ -1,10 +1,9 @@
-import requests
-import random
-from bs4 import BeautifulSoup as soup
-import csv
-import os
-import concurrent.futures
-import time
+import requests # used to submit web requests
+import random # used for shuffling headers when acquiring response to try and avoid err 503/403 
+from bs4 import BeautifulSoup as soup # used for distilling web pages 
+import concurrent.futures # used for paralellism of submitting web requests
+import csv # used for breaking down export file
+import time # used for waiting to avoid getting shut out for bot behaviour
 
 def get_headers(): # cycles headers to imitate a more human user-agent
     user_agent_list = [
@@ -21,39 +20,42 @@ def get_headers(): # cycles headers to imitate a more human user-agent
     "cache-control": "max-age=0",
     "user-agent": user_agent,
     }
+    # note that above may not be enough to get around other websites anti-bot measures (for repeat calls)
+    # however amazon is vast and callous enough to not really care so this generally works for our purposes
     return headers
 
-def get_response(url,drop_headers = False):
-    if drop_headers:
-        response = requests.get(url)
-    else:
-        response = requests.get(url, headers=get_headers())
+def get_response(url):
+    response = requests.get(url, headers=get_headers())
     if response.status_code == 200:
         data = response.text
         return(data)
-    elif response.status_code == 503:
-        time.sleep(1)
+    retry = 0
+    while response.status_code == 503: 
+        retry += 1
+        time.sleep(0.5) # sleep may be unnecesseary but helps space out requests if being denied
         response = requests.get(url, headers=get_headers())
-        data = response.text
-        return(data)
+        if response.status_code ==200:
+            data = response.text
+            return(data)
+        if retry > 5:
+            print(f'\nFailed to get a response from {url} after 5 retries {response.status_code},{response.reason}\n')
+            return ConnectionRefusedError
         # shuffles headers and tries again if error 503 (bot denial)
+        # only retries 5 times 
     else:
-        print(f'Error {response.status_code} occured at : {url} Reason: {response.reason}' )
+        print(f'\nError {response.status_code} occured at : {url} Reason: {response.reason}\n' )
         return ConnectionError
 
-def get_tbr(path): # provide with path to exported storygrpah data(manage account > export storygraph library)
-    try:    
-        with open(path,encoding = "utf8" ) as file:
-            tbr = []
-            reader = csv.DictReader(file)
-            for row in reader:
-                if row['Read Status'] == 'to-read':
-                    x = f'{row['Title']} by {row['Authors']}'
-                    tbr.append(x)
-        print("To-read list found!")
-        return(tbr)
-    except:
-        return Exception
+def get_tbr(path): 
+    with open(path) as file:
+        tbr = []
+        cmplxlist = []
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['Read Status'] == 'to-read':
+                x = f'{row['Title']} by {row['Authors']}'
+                tbr.append(x) 
+    return(tbr)
 
 def get_asin_url(item):
     name = item.replace(" ","+")
@@ -63,7 +65,7 @@ def get_asin_url(item):
     try:
         data = soup(get_response(url),'html.parser')
     except:
-        return
+        return 
     asinlist = data.select('div[class*="s-asin"]')
     # need to use css selector style here as formatting can be different between different items
     try:
@@ -74,29 +76,29 @@ def get_asin_url(item):
         asin = asin.replace('"',"")
         asin = asin.strip()
     except:
-        print(f"\nCould not find asin at {url}")
+        print(f"\nCould not find asin at {url}\n")
         return
     ASINurl = f'https://www.amazon.co.uk/exec/obidos/ASIN/{asin}'
     return(ASINurl)
 
-def get_un_availability(name): # would like to try and do an audible plus availbility too but checking audible+ catalogue feels obtuse
+def get_availability(name):
     ASINUrl = get_asin_url(name)
-    if ASINUrl == None:
-        return
     try:
         data = soup(get_response(ASINUrl),'html.parser')
     except:
-        return
+        return('UNAVAILABLE')
     prices = data.find_all("span",{"class":"slot-price"})
     for x in prices:
         item = str(x)
         try:
+            # look for instance of no cost and mention of kindle unlimited in same block
             item.index('"£0.00"')
             item.index('aria-label="With Kindle Unlimited"')
             return(f'{name} : {ASINUrl}')  
         except:
             continue
-    return 
+    return('UNAVAILABLE')
+
 
 def get_tbr_unlimited(path):
     try:
@@ -106,30 +108,49 @@ def get_tbr_unlimited(path):
         return
     chunkedtbr = [tbr[x:x+30] for x in range(0,len(tbr),30)] # chunk response into smaller lists
     available = []
+    count = 0
+    total = len(tbr)
     for tbri in chunkedtbr:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            ASINUrls = [executor.submit(get_un_availability,item) for item in tbri]
+            ASINUrls = [executor.submit(get_availability,item) for item in tbri]
             for x in concurrent.futures.as_completed(ASINUrls):
+                count += 1
+                print("\033[A                             \033[A") # replaces previous line. This prevents overwiting of any errors that get reported too
+                print("[" + ("="*round(count/total * 100)) + ("_"*(100 - round(count/total * 100))) +"]") # progress bar in console 
                 available.append(x.result())
-        time.sleep(3)
+        time.sleep(0.5)
     return available
 
-path = "ENTER PATH TO SG DATA HERE"
-# note that it may be a good idea to avoid spaces along file path and to escape any \ by doubling them up (\\) or replace with /
-path = 'C:/Software/StorygraphTBR/abbydata.csv'
-available = get_tbr_unlimited(path)
-try:
-    os.remove("output.txt") 
-except FileNotFoundError:
-    pass
-count = 0
-available.sort()
-with open("output.txt","a") as file:
-    for x in available:
-        if x != None:
-            count += 1
-            print(x)
-            file.write(f'{x}\n') # appends to output file
-    print(f'{count} results found from {len(get_tbr(path))} books')
-    file.write(f'{count} results found from {len(get_tbr(path))} books')
-print("Saved to output.txt")
+def main(): 
+    active = True
+    while active: # get path
+        path = input("Enter path to Storygraph export\n")
+        try:
+            get_tbr(path)
+            active = False
+            break
+        except Exception as ex:
+            print(f"Please enter a valid file path to a storygraph export Error Message : {ex})")
+            print("You can export the data by going to 'Manage Account' and scrolling to 'Export Storygraph Library'")
+    print("Export recognised!\n")
+    available = get_tbr_unlimited(path)
+    try:
+        dest = path[:path.rindex('/')]
+    except:
+        dest = path[:path.rindex('\\')+1]
+    count = 0
+    dest = dest + "KindleCollection.txt"
+    with open(dest,"w") as file: 
+        for x in available:
+            if x != "UNAVAILABLE":
+                print(x)
+                file.write(x + ' \n')
+                count += 1
+            else:
+                continue
+        print(f"{count} results found ({len(available)} books checked)")
+        file.write(f'{count} results found from {len(get_tbr(path))} books')
+    print(f"File saved to {dest}")
+    input("\nHit return/enter key to close this window")
+
+main()
